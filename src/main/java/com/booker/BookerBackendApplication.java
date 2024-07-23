ffaf5974e250e6c68ddfa6d7a8d81be6e0f1d676
nyximos <nyximos@gmail.com>
2024-07-23 13:33:20 +0900

    first commit

---
+++ src/main/java/com/booker/BookerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/booker/BookerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

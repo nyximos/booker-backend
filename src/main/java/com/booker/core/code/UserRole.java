ab96479be9b2027bcff0cc1dfbe98f4754df7167
nyximos <nyximos@gmail.com>
2024-07-23 13:35:32 +0900

    설정 추가

---
+++ src/main/java/com/booker/core/code/UserRole.java
@@ -0,0 +1,13 @@
+package com.booker.core.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserRole {
+
+    ADMIN,
+    NORMAL_USER
+    ;
+}

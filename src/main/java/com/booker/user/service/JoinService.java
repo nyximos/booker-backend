b74a3452446652c1244a6ab05d1b2ae378bf8fc4
nyximos <nyximos@gmail.com>
2024-07-31 16:00:55 +0900

    refactor: @Transactional

--- src/main/java/com/booker/user/service/JoinService.java
+++ src/main/java/com/booker/user/service/JoinService.java
@@ -4,9 +4,7 @@ import com.booker.core.util.EncryptUtils;
 import com.booker.user.controller.front.model.request.JoinRequestModel;
 import com.booker.user.converter.UserConverter;
 import com.booker.user.persistence.repository.UserEntity;
-import com.booker.user.persistence.repository.UserRepository;
 import com.booker.user.service.dao.UserDao;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +15,6 @@ public class JoinService {
     private final UserDao userDao;
     private final UserConverter userConverter;
 
-    @Transactional
     public void join(JoinRequestModel joinModel) {
         String salt = EncryptUtils.generateSalt();
         UserEntity user = userConverter.convert(joinModel, salt);
--- src/main/java/com/booker/user/service/dao/UserDao.java
+++ src/main/java/com/booker/user/service/dao/UserDao.java
@@ -4,6 +4,8 @@ import com.booker.user.persistence.repository.UserEntity;
 import com.booker.user.persistence.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,10 +15,12 @@ public class UserDao {
 
     private final UserRepository userRepository;
 
+    @Transactional
     public void save(UserEntity user) {
         userRepository.save(user);
     }
 
+    @Transactional(readOnly = true)
     public Optional<UserEntity> findById(String id) {
         return userRepository.findById(id);
     }
